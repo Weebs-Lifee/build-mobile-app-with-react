d8e6762f8247c3a5c9173273c88d19d8ffa595aa
Adin Shobirin <57091939+Weebs-Lifee@users.noreply.github.com>
2024-06-28 00:26:52 +0700

    first commit

--- App.tsx
+++ App.tsx
@@ -76,8 +76,8 @@ function App(): React.JSX.Element {
           style={{
             backgroundColor: isDarkMode ? Colors.black : Colors.white,
           }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
+          <Section title="Title Text">
+            Edit <Text style={styles.highlight}>Adin Shobirin</Text> to awikwok amat this
             screen and then come back to see your edits.
           </Section>
           <Section title="See Your Changes">
